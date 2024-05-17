e7b6cb85e890965ee963a1ccfe3ba91de922bc85
unknown <jamilafzal255@gmail.com>
2024-05-18 00:29:46 +0500

    done

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ import imageUpload from '../controllers/imageUpload.js'
 import upload from '../middlewares/multer.js'
 const routes = express.Router()
 
-routes.post('/api/imageUpload', upload.single("img"), imageUpload)
-
+routes.post('/api/imageupload', upload.single('img'), imageUpload)
 
 export default routes
